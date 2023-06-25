f56fcb059ed0d00466c2c9fa63afd6833acbb078
ljt528 <ljt528@naver.com>
2023-06-26 02:12:49 +0900

    Make PizzaWormGame

--- pizzaGame/tile.js
+++ pizzaGame/tile.js
@@ -12,15 +12,18 @@ class Tile {
     }
 
     renderImg(bg = './pizza.png') {
+        // img 요소를 만드는 생성자 함수입니다.
         const image = new Image(tileSize, tileSize);
         image.src = bg;
         image.addEventListener('load', () => {
+            // drawImage : 이미지를 그립니다. 그려줄 이미지 요소, x축, y축 위치, 가로, 세로 사이즈를 전달합니다.
             ctxBg.drawImage(image, this.posX, this.posY, tileSize, tileSize);
-        });
+        })
     }
-
+    
     // 두 타일의 충돌을 체크합니다.
     collisionCheck(target) {
+        // && 연산자는 첫번째 falsy 값을 가지는 피연산자 혹은 마지막 truthy 값을 가지는 피연산자를 반환합니다.
         return this.col === target.col && this.row === target.row;
     }
 }
--- pizzaGame/worm.js
+++ pizzaGame/worm.js
@@ -10,15 +10,17 @@ class Worm {
     renderWorm() {
         this.wormBody.forEach((item) => {
             item.renderTile();
-        });
+        })
     }
 
+    // 충돌 체크
     collisionCheck(wormHead) {
         // 게임 테두리 boolean
         const leftEdge = (wormHead.col === 0);
         const topEdge = (wormHead.row === 0);
 
-        const rightEdge = (wormHead.col === tileWidth - 1);
+        // 타일 위치 기준점 왼쪽 상단
+        const rightEdge = (wormHead.col === tileWidth - 1); 
         const bottomEdge = (wormHead.row === tileHeight - 1);
 
         // 게임 테두리에 충돌했는지 체크
@@ -28,14 +30,14 @@ class Worm {
         let collisionBody;
         this.wormBody.forEach((item) => {
             if (wormHead.collisionCheck(item)) {
-                collisionBody = true
+                collisionBody = true;
             }
         })
 
-        return collisionEdge || collisionBody
+        return collisionEdge || collisionBody;
     }
 
-
+    // 이동
     moveWorm() {
         // 지렁이의 현재 머리
         const head = this.wormBody[0];
@@ -46,33 +48,41 @@ class Worm {
         // 다음 방향으로 지렁이를 컨트롤 합니다.
         this.dir = this.dirNext;
 
+        // 이동 방향에 따른 벌래의 머리 랜더링 위치 설정. 기존 머리가 있던 위치에서 위, 아래, 왼쪽, 오른쪽으로 새로운 머리 생성
         if (this.dir === "ArrowRight") {
             newHead = new Tile(head.col + 1, head.row);
-        } else if (this.dir === 'ArrowDown') {
+        } else if (this.dir === "ArrowDown") {
             newHead = new Tile(head.col, head.row + 1);
-        } else if (this.dir === 'ArrowLeft') {
+        } else if (this.dir === "ArrowLeft") {
             newHead = new Tile(head.col - 1, head.row);
-        } else if (this.dir === 'ArrowUp') {
+        } else if (this.dir === "ArrowUp") {
             newHead = new Tile(head.col, head.row - 1);
         }
 
 
+        // 머리와 몸통이 충돌 시 GameOver
         if (this.collisionCheck(newHead)) {
             renderGameOver();
         }
 
+        // 머리에 새로운 머리를 추가
         this.wormBody.unshift(newHead);
 
+        // 머리와 피자가 충돌하면 1.점수가 올라감 / 2. 피자가 새로운 위치로 이동
         if (newHead.collisionCheck(pizza.pos)) {
             ctxBg.clearRect(0, 0, cWidth, cHeight);
+            score++;
             pizza.movePizza();
+        } else {
+            this.wormBody.pop();
         }
-
-        this.wormBody.pop();
     }
 
-    //  사용자가 입력한 방향에 따라 지렁이의 방향을 결정합니다.
+    // 사용자가 입력한 방향에 따라 지렁이의 방향을 결정합니다.
     checkDirection(dirKey) {
+        console.log(dirKey);
+
+        // 진행 방향의 역방향으로 키보드 입력 불가
         if (this.dir === "ArrowRight" && dirKey === "ArrowLeft") {
             return;
         } else if (this.dir === "ArrowDown" && dirKey === "ArrowUp") {
